af935c82350b56e1500f95249faf7e714f8257a6
Mohamed-Atif11 <mhmmd.attff@gmail.com>
2025-04-26 01:53:23 +0300

    add product

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
 // alert("Welcome , نحن نعمل على تطوير الموقع");
+const music = document.getElementById("bgMusic");
+
+document.getElementById("showAlert").addEventListener("click", () => {
+  Swal.fire({
+    title: "مرحبًا بك!",
+    text: "هل ترغب في سماع موسيقى كلاسيكية؟",
+    icon: "info",
+    showCancelButton: true,
+    confirmButtonText: "شغّل الموسيقى 🎶",
+    cancelButtonText: "لأ شكراً",
+  }).then((result) => {
+    if (result.isConfirmed) {
+      music.play();
+    }
+  });
+});
--- js/product.js
+++ js/product.js
@@ -1,1 +1,34 @@
 // alert("قريبا ، نحن نعمل على تطوير الموقع");
+var mood = document.querySelectorAll(".mood");
+var $DarkMode = document.querySelector("#Dark-Mode");
+var $lightMode = document.querySelector("#light-Mode");
+var bgBody = document.querySelector(".bg-body");
+// Dark Mode
+$DarkMode.addEventListener("click", () => {
+  mood.forEach((x) => {
+    x.style.color = "white";
+    x.style.background = "black";
+  });
+});
+// light mode
+$lightMode.addEventListener("click", () => {
+  mood.forEach((x) => {
+    x.style.color = "black";
+    x.style.background = "white";
+  });
+});
+// whats App
+const phoneNumber = "+201022111175"; // رقمك بدون +
+
+document.querySelectorAll(".whatsapp-btn").forEach((button) => {
+  button.addEventListener("click", () => {
+    const card = button.closest(".card"); // بنرجع لأقرب كارد
+    const productName = card.querySelector("h5").innerText; // بنجيب اسم المنتج من ال h5
+
+    const message = `مرحبًا، أريد الاستفسار عن المنتج: ${productName}`;
+    const url = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(
+      message
+    )}`;
+    window.open(url, "_blank");
+  });
+});
